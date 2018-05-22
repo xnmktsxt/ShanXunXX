eda40702382072df474dd4ac3bf9e91303f51dee
xnmktsxt <yifsuc@gmail.com>
2018-05-22 20:42:00 +0800

    get password

--- app/src/main/java/com/example/ming/shanxunxx/MainActivity.java
+++ app/src/main/java/com/example/ming/shanxunxx/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.ming.shanxunxx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage("1065930051", "mm");
+            }
+        });
+    }
 
+    public void sendMessage(String phoneNumber, String message){
+        android.telephony.SmsManager smsManager =
+                android.telephony.SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
     }
 }
